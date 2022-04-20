07542479fdb38865095be0fef80fb0febf24268d
Erik Holmberg <b21eriho@student.his.se>
2022-04-20 15:52:48 +0200

    Tell the second screen to get the extra "TITLE" and set the text of it's textview to that extra when the sceen is opened

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String title = getIntent().getStringExtra("TITLE");
+        TextView titleView = findViewById(R.id.title);
+        titleView.setText(title);
     }
 }
