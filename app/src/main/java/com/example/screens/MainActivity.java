9b9730554b612c17c6319ee323a1f5bd480e983b
Erik Holmberg <b21eriho@student.his.se>
2022-04-20 15:41:45 +0200

    Add eventlistener to button in first screen that runs self-defined function. This function creates an intent from it's own intent to SecondActivity with an extra that called title and then starts an activity via this intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonClicked();
+            }
+        });
+    }
+
+    private void onButtonClicked(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("TITLE", "App Title");
+        startActivity(intent);
+
     }
 }
